35641b755c9e21368bf99bdcd95ea4bd1a4fa372
Ezeki Zibzibadze <ez@jok.io>
2015-07-21 17:40:00 +0400

    Added notifications service

---
+++ AltaSoft.Notifications.Service/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltaSoft.Notifications.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
