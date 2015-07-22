99437fbc4dce1c5dc79cb7aa53ab2524a4b13ee9
Ezeki Zibzibadze <ez@jok.io>
2015-07-22 15:35:35 +0400

    added SendGrid provider class

---
+++ AltaSoft.Notifications.Service/ProviderManagers/SendGridProviderManager.cs
@@ -0,0 +1,25 @@
+﻿using AltaSoft.Notifications.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AltaSoft.Notifications.DAL;
+
+namespace AltaSoft.Notifications.Service.ProviderManagers
+{
+    public class SendGridProviderManager : IProviderManager
+    {
+        public int Id
+        {
+            get { return 1; }
+        }
+
+        public async Task<ProviderProcessResult> Process(Message message)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            return new ProviderProcessResult { IsSuccess = true };
+        }
+    }
+}
