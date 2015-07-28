0b23412747597b74eff4dee788b6faef4da56e50
Ezeki Zibzibadze <ez@jok.io>
2015-07-28 15:52:37 +0400

    added more pages

---
+++ AltaSoft.Notifications.Web/Controllers/MessageController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AltaSoft.Notifications.DAL;
+using AltaSoft.Notifications.DAL.Context;
+
+namespace AltaSoft.Notifications.Web.Controllers
+{
+    public class MessageController : Controller
+    {
+        private MainDbContext db = new MainDbContext();
+
+        // GET: Message
+        public ActionResult Index()
+        {
+            var messages = db.Messages.Include(m => m.Provider).Include(m => m.User);
+            return View(messages.ToList());
+        }
+
+        // GET: Message/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: Message/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name");
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Message/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserId,ProviderId,Subject,Content,RetryCount,ErrorMessage,ErrorDetails,State,Priority,ProcessDate,ProcessingDuration,RegDate,LastUpdateDate,RowVersion")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name", message.ProviderId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", message.UserId);
+            return View(message);
+        }
+
+        // GET: Message/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name", message.ProviderId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", message.UserId);
+            return View(message);
+        }
+
+        // POST: Message/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserId,ProviderId,Subject,Content,RetryCount,ErrorMessage,ErrorDetails,State,Priority,ProcessDate,ProcessingDuration,RegDate,LastUpdateDate,RowVersion")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProviderId = new SelectList(db.Providers, "Id", "Name", message.ProviderId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", message.UserId);
+            return View(message);
+        }
+
+        // GET: Message/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Message/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Message message = db.Messages.Find(id);
+            db.Messages.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AltaSoft.Notifications.Web/Controllers/ProviderController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AltaSoft.Notifications.DAL;
+using AltaSoft.Notifications.DAL.Context;
+
+namespace AltaSoft.Notifications.Web.Controllers
+{
+    public class ProviderController : Controller
+    {
+        private MainDbContext db = new MainDbContext();
+
+        // GET: Provider
+        public ActionResult Index()
+        {
+            return View(db.Providers.ToList());
+        }
+
+        // GET: Provider/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provider);
+        }
+
+        // GET: Provider/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Provider/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,RegDate,LastUpdateDate,RowVersion")] Provider provider)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Providers.Add(provider);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(provider);
+        }
+
+        // GET: Provider/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provider);
+        }
+
+        // POST: Provider/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,RegDate,LastUpdateDate,RowVersion")] Provider provider)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(provider).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(provider);
+        }
+
+        // GET: Provider/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(provider);
+        }
+
+        // POST: Provider/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Provider provider = db.Providers.Find(id);
+            db.Providers.Remove(provider);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
