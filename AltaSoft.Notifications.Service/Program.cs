59c3e8c96194fb518c4b686521ce54a569db2c17
Ezeki Zibzibadze <ez@jok.io>
2015-07-28 15:03:01 +0400

    added Windows Service Installation stuff

--- AltaSoft.Notifications.Service/Program.cs
+++ AltaSoft.Notifications.Service/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AltaSoft.Notifications.Service.Common;
+using AltaSoft.Notifications.Service.Service;
 using Microsoft.Owin.Hosting;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,11 +19,18 @@ namespace AltaSoft.Notifications.Service
     {
         static void Main(string[] args)
         {
+            if (!Environment.UserInteractive)
+            {
+                ServiceBase.Run(new[] { new NotificationsService() });
+                return;
+            }
+
+
             var url = ConfigurationManager.AppSettings["SignalrUrl"];
             var worker = new WorkerManager(DAL.MessagePriority.Normal);
 
             worker.Start();
-            
+
             using (WebApp.Start(url))
             {
                 Console.WriteLine("Server running on {0}", url);
---
+++ AltaSoft.Notifications.Service/Service/NotificationsService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltaSoft.Notifications.Service.Service
+{
+    partial class NotificationsService : ServiceBase
+    {
+        WorkerManager worker;
+        IDisposable webApp;
+
+        public NotificationsService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            var url = ConfigurationManager.AppSettings["SignalrUrl"];
+            worker = new WorkerManager(DAL.MessagePriority.Normal);
+
+            worker.Start();
+            webApp = WebApp.Start(url);
+        }
+
+        protected override void OnStop()
+        {
+            worker.Stop();
+            webApp.Dispose();
+        }
+    }
+}
